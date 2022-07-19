ad4deb0d6f5b3e620d52e920c4406785a21fa94e
thuannguyen88 <getaholdofthuan@gmail.com>
2022-07-19 12:45:00 +0100

    add links to shop, navigation and home. Also loaded lazy kitchen logo to assets

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import Categories from "./components/categories/categories.component";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return <div>I am a shop</div>;
+};
 
 const App = () => {
-  // initialise variable array, array that reflects what we have in categories
-  const categories = [
-    {
-      id: 1,
-      title: "High Protein",
-      imageUrl: "https://via.placeholder.com/150/771796",
-    },
-    {
-      id: 2,
-      title: "Five Ingredients",
-      imageUrl: "https://via.placeholder.com/150/771796",
-    },
-    {
-      id: 3,
-      title: "Low Carb",
-      imageUrl: "https://via.placeholder.com/150/771796",
-    },
-    {
-      id: 4,
-      title: "Monthly Meals",
-      imageUrl: "https://via.placeholder.com/150/771796",
-    },
-    {
-      id: 5,
-      title: "Plant Based",
-      imageUrl: "https://via.placeholder.com/150/771796",
-    },
-  ];
-
-  // whenever we map through things we want to use the id
-  return <Categories key={categories.id} categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
