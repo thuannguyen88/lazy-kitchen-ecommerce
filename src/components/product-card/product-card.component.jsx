95702f5b5e6f9edbdc580a4b84e1c027f4139698
thuannguyen88 <getaholdofthuan@gmail.com>
2022-08-08 13:55:13 +0100

    created product card and styling with display grid to display products

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,1 +1,21 @@
 import "./product-card.styles.scss";
+
+import Button from "../button/button.component";
+
+const ProductCard = ({ product }) => {
+  // destructure of the product what I need
+  const { name, price, imageUrl } = product;
+
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <Button>Add to Cart</Button>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,19 @@
 import { useContext } from "react";
+import "./shop.styles.scss";
 
 import { ProductsContext } from "../../contexts/products.context";
+
+import ProductCard from "../../components/product-card/product-card.component";
 
 const Shop = () => {
   // get the products context
   const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {products.map(({ id, name }) => (
+    <div className="products-container">
+      {products.map((product) => (
         // the reason why we want key is because we need to provide id for our map
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
+        // pass product as a prop into product card
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
