1c98f167b6d8903f628547b6333e76b7cb7de8f8
thuannguyen88 <getaholdofthuan@gmail.com>
2022-08-12 12:02:16 +0100

    set up cart quantity counter to update cart quantity when cart items changes using reduce

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,18 +6,19 @@ import { ReactComponent as ShoppingCartIcon } from "../../assets/shopping_bag.sv
 import { DropdownContext } from "../../contexts/dropdown.context";
 
 const CartIcon = () => {
-// use DropdownContext to set up toggling function when cart icon is clicked. 
-  const { isDropdownOpen, setIsDropdownOpen } = useContext(DropdownContext);
+  // use DropdownContext to set up toggling function when cart icon is clicked.
+  const { isDropdownOpen, setIsDropdownOpen, cartQuantity } =
+    useContext(DropdownContext);
 
- // dropdown handler function. When cart icon is clicked set isDropdownOpen to the opposite of what it is. So if its true, set it to false. And if its false, set it to true. 
+  // dropdown handler function. When cart icon is clicked set isDropdownOpen to the opposite of what it is. So if its true, set it to false. And if its false, set it to true.
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
   return (
     <div className="cart-icon-container">
-      <ShoppingCartIcon className="cart-icon" onClick={toggleDropdown}/>
-      <span className="item-count">1</span>
+      <ShoppingCartIcon className="cart-icon" onClick={toggleDropdown} />
+      <span className="item-count">{cartQuantity}</span>
     </div>
   );
 };
--- src/contexts/dropdown.context.jsx
+++ src/contexts/dropdown.context.jsx
@@ -1,50 +1,71 @@
-import { createContext, useState} from "react";
+import { createContext, useState, useEffect } from "react";
 
 // one of the most CHALLENGING functionality to wrap my head around
-// function that allows us to take in an array of cart items, and some product object with an additional quantity field. 
+// function that allows us to take in an array of cart items, and some product object with an additional quantity field.
 const addCartItem = (cartItems, productToAdd) => {
-   // find if cartItems contains productToAdd
-   // .find you pass in a callback that receives each element in an array and we're expecting a boolean value that returns the cartItem that returns true
-   // return us boolean true if the cartItem has an id value that matches the productToAdd id value
-const existingCartItem = cartItems.find((cartItem)=> cartItem.id === productToAdd.id)
+  // find if cartItems contains productToAdd
+  // .find you pass in a callback that receives each element in an array and we're expecting a boolean value that returns the cartItem that returns true
+  // return us boolean true if the cartItem has an id value that matches the productToAdd id value
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
 
-   // If existingCartItem is found, increment quantity
-// we map over all the cartItems if there is an existingCartItem, and create a new object for the cartItem that has same id as productToAdd spreading the values of the cartItem and incrementing the quantity value by 1. 
-// if the cartItem.id does not match the productToAdd.id we do nothing to that specific item and return
-if (existingCartItem) {
-   return cartItems.map((cartItem) => cartItem.id === productToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1} : cartItem)
-}
+  // If existingCartItem is found, increment quantity
+  // we map over all the cartItems if there is an existingCartItem, and create a new object for the cartItem that has same id as productToAdd spreading the values of the cartItem and incrementing the quantity value by 1.
+  // if the cartItem.id does not match the productToAdd.id we do nothing to that specific item and return
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
 
-// if existingCartItem doesnt exist in cartItems
-   // return new array with modified cartItems/ new cart item
-   // if new product, create a new array and spread all existing cart items and the new product we want to add and the quantity of one
-   return [...cartItems, {...productToAdd, quantity: 1}]
-}
-
+  // if existingCartItem doesnt exist in cartItems
+  // return new array with modified cartItems/ new cart item
+  // if new product, create a new array and spread all existing cart items and the new product we want to add and the quantity of one
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 // create context to toggle on and off dropdown menu for shopping cart
 // pass the value we want to access
 // we want to access a boolean
+
+// track the count of total quantity - everytime the cartItems array changes, we are updating the quantity so we can use useEffect to keep track of changes
 export const DropdownContext = createContext({
   isDropdownOpen: false,
   setIsDropdownOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartQuantityCount: 0,
 });
 
 // create provider for context
 export const DropdownProvider = ({ children }) => {
   // set up the state for toggling with initialised value of false
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([])
+  const [cartItems, setCartItems] = useState([]);
+  const [cartQuantity, setCartQuantity] = useState(0);
 
   // a function that takes a product we want to add cart, and adds it to cartItems
   const addItemToCart = (productToAdd) => {
-   setCartItems(addCartItem(cartItems, productToAdd))
-  }
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  // anytime the cartItems array changes, we want to recalculate the total quantity in cart
+  useEffect(() => {
+    // reduce takes two arguments, first is the callback and the second is the starting value. starting with a value of 0, we will go through each cartItem and check the quantity and add it to the currentTotal
+    const totalCartQuantity = cartItems.reduce(
+      (currentTotal, currentCartItem) =>
+        currentTotal + currentCartItem.quantity,
+      0
+    );
+
+    setCartQuantity(totalCartQuantity);
+  }, [cartItems]);
 
   // generate the values to be exported
-  const value = { isDropdownOpen, setIsDropdownOpen, addItemToCart, cartItems };
+  const value = { isDropdownOpen, setIsDropdownOpen, addItemToCart, cartItems, cartQuantity };
 
   return (
     <DropdownContext.Provider value={value}>
